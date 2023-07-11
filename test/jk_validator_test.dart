e9081f9330a35f2169a4ea0d997928c36db5837b
kawan97 <pshtiwankawan@gmail.com>
2023-07-11 08:44:35 -0700

    adding test and example code

--- test/jk_validator_test.dart
+++ test/jk_validator_test.dart
@@ -3,10 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:jk_validator/jk_validator.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('test isEmail', () {
+    expect(isEmail('fsdfdsfds'), false);
+    expect(isEmail('dsadsa@dasdas'), false);
+    expect(isEmail('dads@fdsfsd.com'), true);
   });
 }
